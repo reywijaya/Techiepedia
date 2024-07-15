a99815dc941cb200c96a46b3e13e9d557b8d74ab
Renaldy Wijaya Putra <renaldywijayaputra@gmail.com>
2024-07-15 13:46:34 +0700

    feat: add root location path for upload avatar

--- src/main/java/com/enigmacamp/tokonyadia/service/FileStorageService.java
+++ src/main/java/com/enigmacamp/tokonyadia/service/FileStorageService.java
@@ -3,5 +3,5 @@ package com.enigmacamp.tokonyadia.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorageService {
-    String storeFile(MultipartFile file);
+    void storeFile(MultipartFile file);
 }
--- src/main/java/com/enigmacamp/tokonyadia/service/impl/FileStorageServiceImpl.java
+++ src/main/java/com/enigmacamp/tokonyadia/service/impl/FileStorageServiceImpl.java
@@ -9,19 +9,31 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 @Service
 public class FileStorageServiceImpl implements FileStorageService {
     private static final Logger log = LoggerFactory.getLogger(FileStorageServiceImpl.class);
 
-    @Override
-    public String storeFile(MultipartFile file) {
+    private final Path rootLocation;
+
+    public FileStorageServiceImpl() {
+        this.rootLocation = Path.of("assets/images/");
         try {
-            Path target = Path.of("assets/images/" + file.getOriginalFilename());
-            Files.copy(file.getInputStream(), target);
+            Files.createDirectories(rootLocation);
         } catch (IOException e) {
-            log.info("Failed to store file {}", file.getOriginalFilename(), e);
+            log.error(e.getMessage());
         }
-        return "Successfully stored file " + file.getOriginalFilename();
+    }
+
+    @Override
+    public void storeFile(MultipartFile file) {
+        try {
+            Path target = rootLocation.resolve(Objects.requireNonNull(file.getOriginalFilename()));
+            Files.copy(file.getInputStream(), target, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            log.error("Failed to store file {}", file.getOriginalFilename(), e);
+        }
     }
 }
